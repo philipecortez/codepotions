7e241d636ca3da71a06af004405e99e29a691024
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-05-08 18:41:43 -0300

    add font-awesome

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,31 @@
 import React from "react"
+import Link from 'gatsby-link'
 
 const Nav = () => (
   <nav className="blog-nav">
     <h1>Code Potions *</h1>
     <div className="hb-init">
-     <a id="hb-menu">
-      <i className="fas fa-bars"></i>
+     <a href="#" id="hb-menu">
+        <i className="fas fa-bars"></i>
      </a>
     </div>
     <div className="hb-buttons toggle-content">
       <ul>
-        <li><a href="#">Contact</a></li>
-        <li><a href="https://www.linkedin.com/in/philipe-cortez/">Resume</a></li>
-        <li><a href="#" >Videos</a></li>
+        <li>
+          <Link to="#">
+            Contact
+          </Link>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/philipe-cortez/">
+            Resume
+          </a>
+        </li>
+        <li>
+          <Link to="#">
+            Videos
+          </Link>
+        </li>
       </ul>
     </div>
   </nav>
