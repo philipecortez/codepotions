c46a4e9ba247bc256b85f7269b2ffdbb3045bc21
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-05-08 19:09:34 -0300

    fix mobile nav bar toggler

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,32 @@
 import React from "react"
 import Link from 'gatsby-link'
 
+const toggleMobalBar = () => {
+    let buttons = document.getElementsByClassName("hb-buttons")[0]
+
+    console.log(buttons)
+    if (buttons.classList.contains("toggle-content")) {
+      buttons.toggleAttribute('hidden')
+      setTimeout(() => {
+       buttons.classList.toggle("toggle-content")
+       buttons.classList.toggle("is-visible")
+      }, 100)
+    } else {
+      buttons.classList.toggle("is-visible")
+      buttons.classList.toggle("toggle-content")
+      buttons.toggleAttribute('hidden')
+    }
+}
+
 const Nav = () => (
   <nav className="blog-nav">
     <h1>Code Potions *</h1>
     <div className="hb-init">
-     <a href="#" id="hb-menu">
-        <i className="fas fa-bars"></i>
-     </a>
+      <a onClick={() => toggleMobalBar()} id="hb-menu">
+         <i className="fas fa-bars"></i>
+      </a>
     </div>
-    <div className="hb-buttons toggle-content">
+    <div className="hb-buttons toggle-content" hidden={true}>
       <ul>
         <li>
           <Link to="#">
