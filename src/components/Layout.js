4e5fd29631b7198a7b911aa1aeb67ac7bd60fac4
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-05-08 17:35:07 -0300

    change main tag class and receice metadata

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,20 @@
 
 import React, { Component } from 'react'
 
+import './application.css'
 import Nav from './Nav'
 import Header from './Header'
 import Footer from './Footer'
 
 class Layout extends Component {
   render() {
-    const { children } = this.props
+    const { children, data } = this.props
 
     return (
       <>
         <Nav />
         <Header />
-        <main>{children}</main>
+        <main className="blog-content">{children}</main>
         <Footer />
       </>
     )
