441b3338d62133f8e59e67a7f1d5c4a9aa67430d
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-05-10 01:21:36 -0300

    improve footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => (
   <footer className="blog-footer">
     <ul className="social-media">
       <li><a href="https://github.com/philipecortez"><i className="fab fa-github"></i>Github</a></li>
-      <li><a href="https://www.linkedin.com/in/philipe-cortez/"><i className="fab fa-linkedin"></i>Linkedin</a></li>
-      <li><a href="#"><i className="fab fa-bitcoin"></i>Donate</a></li>
+      <li><a href="https://www.linkedin.com/in/philipe-cortez/"><i className="fab fa-linkedin-in"></i>Linkedin</a></li>
+      <li><a href="#"><i className="fab fa-btc"></i>Donate</a></li>
     </ul>
     <p>All Rights Reserved &#9400;</p>
   </footer>
